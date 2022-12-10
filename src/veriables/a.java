a72d713ccd91a5715fc0ff4d3bad41a50d785d95
Hasan <Hasan@10.44.104.22>
2022-12-10 14:33:25 +0300

    commit

---
+++ src/veriables/a.java
@@ -0,0 +1,14 @@
+package veriables;
+
+public class a {
+
+	public static void main(String[] args) {
+		int ürünFiyatı = 350 ;
+		String fiş ="etiket fiyatı :" ;
+		System.out.println(fiş + ürünFiyatı);
+		System.out.println(fiş + ürünFiyatı);
+		System.out.println(fiş + ürünFiyatı);
+		System.out.println(fiş + ürünFiyatı);
+	}
+
+}
